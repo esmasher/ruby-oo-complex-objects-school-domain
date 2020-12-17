fb53544880360a672ecf4a14fa9115c59af236d2
esmasher <esmasherr@gmail.com>
2020-12-17 09:10:27 -0600

    commit tree 73177b7b760a9d2a3473b6844b118c592989ee9d

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+class School
+    attr_accessor :name, :grade
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+
+    def add_student(name, grade)
+        roster[grade] ||= []
+        roster[grade] << name
+
+
+
+        #roster[grade] ||= []
+        #roster[grade] << student_name
+      end
+
+end
